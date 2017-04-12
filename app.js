f3c2300c5789c00efa6d91be6bbab54f9ff8404a
brreex <berhanuasa@gmail.com>
2017-04-12 09:11:10 -0600

    route change for root

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/', message);
 app.use('/users', users);
 app.use('/message',message);
 
